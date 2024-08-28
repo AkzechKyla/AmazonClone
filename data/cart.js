add247bab7935f333be7cd834a61f61e4339baf5
Kyla <kflmorcillos@gmail.com>
2024-08-28 17:22:34 +0800

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,21 @@
-export const cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+  }, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  }];
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
-    const itemInCart = cart.find(item => item.productID === productId);
+    const itemInCart = cart.find(item => item.productId === productId);
     const itemQuantity = document.querySelector(`.js-quantity-selector-${productId}`);
 
     if (itemInCart) {
@@ -16,8 +24,10 @@ export function addToCart(productId) {
       cart.push({
         productId,
         quantity: Number(itemQuantity.value)
-    });
-  }
+      });
+    }
+
+  saveToLocalStorage();
 }
 
 export function deleteProductToCart(productId) {
@@ -26,4 +36,6 @@ export function deleteProductToCart(productId) {
       cart.splice(cart.indexOf(cartItem), 1);
     }
   });
+
+  saveToLocalStorage();
 }
