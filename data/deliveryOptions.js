ff2244e49820014ed5fd51cb4bf661e94c462263
Kyla <kflmorcillos@gmail.com>
2024-08-30 13:13:03 +0800

    Change delivery days

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -1,13 +1,13 @@
 export const deliveryOptions = [{
     id: '1',
-    deliveryDays: 7,
+    deliveryDays: 11,
     priceCents: 0
 }, {
     id: '2',
-    deliveryDays: 3,
+    deliveryDays: 5,
     priceCents: 499
 }, {
     id: '3',
-    deliveryDays: 1,
+    deliveryDays: 3,
     priceCents: 999
 }];
