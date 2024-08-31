246af9a8b6eb6929b579c0a7d26ff59ac1115e9b
Kyla <kflmorcillos@gmail.com>
2024-08-31 17:25:07 +0800

    Improve indentation

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -25,46 +25,46 @@ function generateOrderSummary() {
     });
 
     cartSummaryHTML += `
-          <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
-            <div class="delivery-date">
-              Delivery date: ${deliveryDate}
+      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+        <div class="delivery-date">
+          Delivery date: ${deliveryDate}
+        </div>
+
+        <div class="cart-item-details-grid">
+          <img class="product-image"
+            src="${matchingProduct.image}">
+
+          <div class="cart-item-details">
+            <div class="product-name">
+              ${matchingProduct.name}
             </div>
-
-            <div class="cart-item-details-grid">
-              <img class="product-image"
-                src="${matchingProduct.image}">
-
-              <div class="cart-item-details">
-                <div class="product-name">
-                  ${matchingProduct.name}
-                </div>
-                <div class="product-price">
-                  $${formatCurrency(matchingProduct.priceCents)}
-                </div>
-                <div class="product-quantity">
-                  <span>
-                    Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
-                  </span>
-                  <span class="update-quantity-link link-primary" data-product-id="${matchingProduct.id}">
-                    Update
-                  </span>
-                  <input type="number" max="1000" min="1" value="${cartItem.quantity}" class="quantity-input js-quantity-input-${matchingProduct.id}">
-                  <span class="save-quantity-link link-primary js-save-quantity-link-${matchingProduct.id}">Save</span>
-                  <span class="delete-quantity-link link-primary" data-product-id="${matchingProduct.id}">
-                    Delete
-                  </span>
-                </div>
-              </div>
-
-              <div class="delivery-options">
-                <div class="delivery-options-title">
-                  Choose a delivery option:
-                </div>
-                ${generateDeliveryOptionsHTML(matchingProduct, cartItem)}
-              </div>
+            <div class="product-price">
+              $${formatCurrency(matchingProduct.priceCents)}
+            </div>
+            <div class="product-quantity">
+              <span>
+                Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
+              </span>
+              <span class="update-quantity-link link-primary" data-product-id="${matchingProduct.id}">
+                Update
+              </span>
+              <input type="number" max="1000" min="1" value="${cartItem.quantity}" class="quantity-input js-quantity-input-${matchingProduct.id}">
+              <span class="save-quantity-link link-primary js-save-quantity-link-${matchingProduct.id}">Save</span>
+              <span class="delete-quantity-link link-primary" data-product-id="${matchingProduct.id}">
+                Delete
+              </span>
             </div>
           </div>
-        `
+
+          <div class="delivery-options">
+            <div class="delivery-options-title">
+              Choose a delivery option:
+            </div>
+            ${generateDeliveryOptionsHTML(matchingProduct, cartItem)}
+          </div>
+        </div>
+      </div>
+    `
   });
 
   document.querySelector('.order-summary').innerHTML = cartSummaryHTML;
@@ -87,19 +87,19 @@ function generateDeliveryOptionsHTML(matchingProduct, cartItem) {
     }
 
     deliveryOptionsHTML += `
-                <div class="delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
-                  <input type="radio" ${isChecked} class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      ${deliveryDate}
-                    </div>
-                    <div class="delivery-option-price">
-                      ${shippingPrice}
-                    </div>
-                  </div>
-                </div>
-      `;
+      <div class="delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
+        <input type="radio" ${isChecked} class="delivery-option-input"
+          name="delivery-option-${matchingProduct.id}">
+        <div>
+          <div class="delivery-option-date">
+            ${deliveryDate}
+          </div>
+          <div class="delivery-option-price">
+            ${shippingPrice}
+          </div>
+        </div>
+      </div>
+    `;
   });
 
   return deliveryOptionsHTML;
