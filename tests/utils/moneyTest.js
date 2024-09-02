e119031cc212373bd134967423ececbd925b357b
Kyla <kflmorcillos@gmail.com>
2024-09-03 07:47:00 +0800

    Move test files to their own directory

--- tests/utils/moneyTest.js
+++ tests/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from '../scripts/utils/money.js';
+import {formatCurrency} from '../../scripts/utils/money.js';
 
 // to create a test suite
 describe('Test Suite: formatCurrency', () => {
